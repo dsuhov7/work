8c6dc29e762c888c14d697be79377532ca95b20a
dsuhov7 <dsuhov7@gmail.com>
2019-09-24 12:02:32 +0300

    1

---
+++ first_prog.py
@@ -0,0 +1,28 @@
+import re
+sl = []
+with open("new.py") as f:
+    while True:
+        s = f.readline()
+        if s == "":
+            break
+        sn = re.sub('^\s,{2,5}',' '*4,s)
+        sn = re.sub("\t"," "*4,s)
+        sl+=sn
+
+
+#with open("new.py") as f:
+ #   while True:
+  #      s = f.readline()
+   #     import re
+    #    sn = re.sub('^\s,{2,5}',' '*4,s)
+#f.tell()
+#f.seek(b)
+     #   snl = []
+      #  snl = snl + sn
+
+
+
+with open("new.py",'w') as f:
+    f.writelines(sl)
+
+
