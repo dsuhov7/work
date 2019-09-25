5b010c53a41d47dff4c5b49a2bf680205a5185c7
dsuhov7 <dsuhov7@gmail.com>
2019-09-25 13:06:02 +0300

    1

---
+++ Alice.py
@@ -0,0 +1,14 @@
+import requests
+import re
+#import BeautifulSoup
+x = requests.get('https://www.cs.cmu.edu/~rgs/alice-I.html')
+#soup = BeautifulSoup(x.text, 'html.parser')
+#soup.find_all('p')
+
+s = x.text
+sp = re.findall(r'\w+',s)
+print(sp)
+#sn = re.sub{'',s}
+
+#print(x.text)
+
